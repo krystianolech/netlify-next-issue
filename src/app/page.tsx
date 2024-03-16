761fce62aa38e27b81c72b83e98061991744e663
Krystian Olech <olhk@viessmann.com>
2024-03-16 18:01:59 +0100

    Change bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
   ).json();
 
   return (
-    <main className="w-screen h-screen flex items-center justify-center bg-gray-400">
+    <main className="w-screen h-screen flex items-center justify-center bg-gray-500">
       <div className="shadow-lg w-40 h-40 bg-white p-4 flex flex-col items-center gap-4">
         <span className="font-semibold">
           Current time form API:
